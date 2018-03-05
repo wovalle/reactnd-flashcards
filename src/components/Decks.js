ef6ce0d5c7268ff2ea90dd0ea3c1431154c6251a
Willy Ovalle <willyovalle16@gmail.com>
2018-03-04 22:01:50 -0400

    Feature: adding onDeckClick

--- src/components/Decks.js
+++ src/components/Decks.js
@@ -9,9 +9,15 @@ class Decks extends React.Component {
     this.props.getDecks();
   }
 
+  onDeckClick = (title) => {
+    return () => {
+      this.props.navigation.navigate('Deck', { deckId: title })
+    }
+  }
+
   render() {
     const DeckItems = this.props.decks.map(({ title, questions }) => (
-      <ListItem style={{ height: 150 }} key={title}>
+      <ListItem style={{ height: 150 }} key={title} onPress={this.onDeckClick(title)}>
         <Body style={{ alignItems: 'center' }}>
           <Text>{title}</Text>
           <Text note>{`${questions.length} card${questions.length !== 1 ? 's' : ''}`}</Text>
