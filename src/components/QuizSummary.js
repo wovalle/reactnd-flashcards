2a9d66e070a68dc0ee6e5c632bfab2a3b07462f9
Willy Ovalle <willyovalle16@gmail.com>
2018-03-07 10:22:13 -0400

    Fix: add back button to Quiz Summary

--- src/components/QuizSummary.js
+++ src/components/QuizSummary.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Container, Content, Text, H1, Button } from 'native-base';
+import { Container, Content, Text, H1, Button, View } from 'native-base';
 import { connect } from 'react-redux';
+import { StyleSheet } from 'react-native';
 
 class QuizSummary extends React.Component {
   static navigationOptions = ({ navigation }) => ({
     title: 'Summary',
   });
+
+  goBack = () => {
+    this.props.navigation.goBack();
+  }
 
   render() {
     const { quiz } = this.props.navigation.state.params;
@@ -15,10 +20,25 @@ class QuizSummary extends React.Component {
       <Container>
         <Content contentContainerStyle={{ flex: 1, alignItems: 'center', paddingTop: 80 }}>
           <H1>{`You got ${quiz.correctQuestions} out of ${questionsQuantity} questions!`}</H1>
+          <View style={styles.buttons_container}>
+            <Button>
+              <Text onPress={this.goBack}>Back to deck</Text>
+            </Button>
+          </View>
         </Content>
       </Container>
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  buttons_container: {
+    marginTop: 140,
+  }
+});
 
 export default QuizSummary;
